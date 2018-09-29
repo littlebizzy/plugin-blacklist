97e5a6e9c4674a80c1804d82ec88a2f00c6de792
Pau <pauiglesias@gmail.com>
2018-09-29 20:00:18 +0200

    Set a flag after activation to check the plugins loaded in next request.

--- core/core.php
+++ core/core.php
@@ -22,24 +22,18 @@ final class Core extends Helpers\Singleton {
 	protected function onConstruct() {
 
 		//add_action('plugins_loaded', [$this, 'pluginsLoaded']);
-		//add_action('activate_plugin', [$this, 'activatePlugin'], 10 ,2); //do_action( 'activate_plugin', $plugin, $network_wide );
+		add_action('activate_plugin', [$this, 'onActivation'], 10 ,2); //do_action( 'activate_plugin', $plugin, $network_wide );
 
 		// Factory object
 		//$this->plugin->factory = new Factory($this->plugin);
 	}
 
 
-	public function activatePlugin($plugin, $network_wide) {
 
-		/* if (function_exists('ntfthp_is_frontend')) {
-			$refFunc = new \ReflectionFunction('ntfthp_is_frontend');
-			error_log($refFunc->getFileName());
-		} */
-
-		// echo 'Unallowed plugin'; // <-- disables plugin activation
-		//error_log($plugin); // file
-		//error_log($network_wide); // boolean
+	public function onActivation($plugin, $network_wide) {
+		update_option('plblst_check_activation', 1, true);
 	}
+
 
 
 	public function pluginsLoaded() {
@@ -49,6 +43,18 @@ final class Core extends Helpers\Singleton {
 
 		//$plugins = wp_get_active_and_valid_plugins();
 		//print_r($plugins);die;
+	}
+
+
+
+	public function notices() {
+
+		?><div class="notice notice-error is-dismissible">
+
+			<p>Plugin not allowed</p>
+
+		</div><?php
+
 	}
 
 
