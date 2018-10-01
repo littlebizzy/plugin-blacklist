cc02745c685cb6a4fb5b019a2e5179a2501816ad
Pau <pauiglesias@gmail.com>
2018-10-01 12:31:16 +0200

    Special transformation of ini files to allow any data type.

--- plugin/blacklist.php
+++ plugin/blacklist.php
@@ -15,17 +15,90 @@ use \LittleBizzy\PluginBlacklist\Helpers;
 final class Blacklist extends Helpers\Singleton {
 
 
-	private $content;
+
+	/**
+	 * Cached config
+	 */
+	private $config;
 
 
 
+	/**
+	 * Read the blacklist file
+	 */
 	public function read($cache = true) {
 
 		// Check content cached
-		if ($cache && isset($this->content)) {
-			return $content;
+		if ($cache) {
+			if (isset($this->config)) {
+				return $this->config;
+			}
 		}
 
+		// Clear any cache
+		$this->config = null;
+
+		// Blacklist file path
+		$path = WP_CONTENT_DIR.'/blacklist.txt';
+		if (!@file_exists($path)) {
+			return false;
+		}
+
+		// Retrieve content
+		$lines = @file_get_contents($path);
+		if (empty($lines)) {
+			return false;
+		}
+
+		// Explode lines
+		$lines = str_replace("\r\n", "\n", $lines);
+		$lines = explode("\n", $lines);
+
+		// Enum lines
+		$lines2 = [];
+		foreach ($lines as $line) {
+
+			// Avoid empty lines
+			$line = trim($line);
+			if ('' === $line || ';' == substr($line, 0, 1)) {
+				continue;
+			}
+
+			// Remove comments
+			$comment = strpos($line, ';');
+			if (false !== $comment) {
+				$line = trim(substr($line, 0, $comment));
+				if ('' === $line) {
+					continue;
+				}
+			}
+
+			// Check section
+			if ('[' != substr($line, 0, 1)) {
+				if (false === strpos($line, '=')) {
+					$line .= ' = 1';
+				}
+			}
+
+			// Sanitized line
+			$lines2[] = $line;
+		}
+
+		// Check lines
+		if (empty($lines2)) {
+			return false;
+		}
+
+		// Cast to string
+		$content = implode("\n", $lines2);
+
+		// Parse ini file via PHP functions
+		if (false === ($this->config = @parse_ini_string($content, true, INI_SCANNER_TYPED))) {
+			return false;
+		}
+
+		// Done
+		return $this->config;
 	}
 
 
